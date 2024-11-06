8d35ed9278bb79945fba359cd1d52a228216c18d
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-06 18:09:32 -0500

    Update Character.cs

--- Models/Character.cs
+++ Models/Character.cs
@@ -20,13 +20,3 @@ public abstract class Character : ICharacter
         Console.WriteLine($"{Name} attacks {target.Name}!");
     }
 }
-
-public abstract class Ability
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-
-    // Navigation property to Characters
-    public virtual ICollection<Character> Characters { get; set; }
-}

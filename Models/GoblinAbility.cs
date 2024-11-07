e65cb4ef52d7d0c0d6fac3f90e2134c6171e48ef
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-06 20:22:52 -0500

    Character Ability Classes

---
+++ Models/GoblinAbility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W9_assignment_template.Models
+{
+    public class GoblinAbility : Ability
+    {
+        public int Taunt { get; set; }
+    }
+}

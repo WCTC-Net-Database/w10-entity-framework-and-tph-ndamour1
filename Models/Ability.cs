d2d42611109fc14a4795514581cb491e099673cc
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-06 20:16:04 -0500

    Ability Class

---
+++ Models/Ability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W9_assignment_template.Models
+{
+    public abstract class Ability
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Navigation property to Characters
+        public virtual ICollection<Character> Characters { get; set; }
+    }
+}
